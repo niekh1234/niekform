92296175906fe7163348333ff9e5321e8d7a937e
Niek Hagen <34717999+niekh1234@users.noreply.github.com>
2022-12-11 20:31:44 +0100

    (feat): form submissions
    * (chore): seed form field data
    
    * (wip): form submissions
    
    * (feat): clean + validate submission

---
+++ lib/form/submission.ts
@@ -0,0 +1,56 @@
+import { Field, FieldType } from '@prisma/client';
+
+export const validateSubmission = (formFields: Field[], submission: any) => {
+  const errors: any = {};
+
+  formFields.forEach((field) => {
+    const fieldName = field.name.toLowerCase();
+
+    if (field.required && !submission[fieldName]) {
+      errors[fieldName] = 'Required';
+    }
+
+    if (field.type === FieldType.EMAIL && submission[fieldName]) {
+      const emailRegex =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+      if (!emailRegex.test(submission[fieldName])) {
+        errors[fieldName] = 'Invalid email';
+      }
+
+      return;
+    }
+
+    if (field.type === FieldType.NUMBER && submission[fieldName]) {
+      const numberRegex = /^[0-9]+$/;
+
+      if (!numberRegex.test(submission[fieldName])) {
+        errors[fieldName] = 'Invalid number';
+      }
+
+      return;
+    }
+  });
+
+  return { errors, valid: Object.keys(errors).length === 0 };
+};
+
+export const cleanSubmission = (formFields: Field[], submission: any) => {
+  const clean: any = {};
+
+  formFields.forEach((field) => {
+    const fieldName = field.name.toLowerCase();
+
+    clean[fieldName] = null;
+
+    if (submission[fieldName]) {
+      clean[fieldName] = submission[fieldName];
+
+      if (field.type === FieldType.CHECKBOX) {
+        clean[fieldName] = !!submission[fieldName];
+      }
+    }
+  });
+
+  return clean;
+};
---
+++ pages/api/f/[id].ts
@@ -0,0 +1,41 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { badRequest, ok, notFound } from 'next-basics';
+import nextConnect from 'next-connect';
+import prisma from 'lib/prisma';
+import { cleanSubmission, validateSubmission } from 'lib/form/submission';
+
+export default nextConnect().post(
+  async (req: NextApiRequest, res: NextApiResponse) => {
+    if (!req.query.id) {
+      return badRequest(res, 'Missing id');
+    }
+
+    // todo honey pot
+
+    const form = await prisma.form.findFirst({
+      where: { id: req.query.id as string },
+      include: { fields: true },
+    });
+
+    if (!form) {
+      return notFound(res, 'Form not found');
+    }
+
+    const { errors, valid } = validateSubmission(form.fields, req.body);
+
+    if (!valid) {
+      return badRequest(res, JSON.stringify({ errors }));
+    }
+
+    const cleaned = cleanSubmission(form.fields, req.body);
+
+    const submission = await prisma.submission.create({
+      data: {
+        formId: form.id,
+        data: cleaned,
+      },
+    });
+
+    return ok(res, submission || {});
+  }
+);
