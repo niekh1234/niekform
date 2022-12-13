d11e2d4fbf95f1eef1c34f9134de270d8aedb055
Niek Hagen <niekhagen@hotmail.com>
2022-12-13 23:09:06 +0100

    (chore): update flash with new react 18 syntax

--- components/App/Flash.tsx
+++ components/App/Flash.tsx
@@ -2,7 +2,7 @@ import { Portal } from '@headlessui/react';
 import { classNames } from 'lib/client/utils';
 import React from 'react';
 import { useEffect, useState } from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot, Root } from 'react-dom/client';
 
 type FlashProps = {
   message: string;
@@ -11,46 +11,43 @@ type FlashProps = {
 };
 
 const backgroundClasses = {
-  success: 'bg-green-200',
-  warning: 'bg-yellow-200',
-  error: 'bg-red-200',
+  success: 'bg-emerald-200',
+  warning: 'bg-yellow-100',
+  error: 'bg-red-100',
 };
 
 const textClasses = {
-  success: 'text-green-900',
+  success: 'text-emerald-900',
   warning: 'text-yellow-900',
   error: 'text-red-900',
 };
 
 export const spawnFlash = (message: string, type: 'success' | 'warning' | 'error') => {
-  const flash = React.createElement(Flash, { message, type, onClose: removeFlash });
-  ReactDOM.render(flash, document.getElementById('niekform-flash'));
+  const containerElement = document.getElementById('niekform-flash');
+
+  if (!containerElement) {
+    return;
+  }
+
+  const root = createRoot(containerElement);
+  const flash = React.createElement(Flash, { message, type, onClose: () => root.unmount() });
+  root.render(flash);
 
   setTimeout(() => {
-    removeFlash();
+    root.unmount();
   }, 5000);
-
-  return removeFlash;
-};
-
-const removeFlash = () => {
-  const el = document.getElementById('niekform-flash');
-
-  if (el) {
-    ReactDOM.unmountComponentAtNode(el);
-  }
 };
 
 const Flash = ({ message, type, onClose }: FlashProps) => {
   return (
     <div
       className={classNames(
-        'fixed top-4 right-4 z-50 rounded-xl p-4 max-w-xl',
+        'fixed top-4 right-4 z-50 rounded-xl p-4 max-w-xl shadow',
         backgroundClasses[type]
       )}
     >
       <div className="flex items-center justify-between">
-        <div className={classNames('pr-2', textClasses[type])}>{message}</div>
+        <div className={classNames('pr-2 text-sm', textClasses[type])}>{message}</div>
         <button className="text-lg font-bold" onClick={onClose}>
           &times;
         </button>
