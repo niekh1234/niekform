2f8017c6536179df09c523851da48f248486f80c
Niek Hagen <niekhagen@hotmail.com>
2022-12-14 21:51:34 +0100

    (feat): custom 404 scaffolding

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+const Custom404 = () => {
+  return (
+    <div className='py-16 text-center'>
+      <h1 className='text-4xl font-black'>404 - Page not found</h1>
+    </div>
+  );
+};
+
+export default Custom404;
