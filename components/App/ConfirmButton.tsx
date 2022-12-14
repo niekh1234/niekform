ccbec30ba069020df775590263ebeff6eae8a479
Niek Hagen <niekhagen@hotmail.com>
2022-12-14 21:51:50 +0100

    (chore): formatting

--- components/App/ConfirmButton.tsx
+++ components/App/ConfirmButton.tsx
@@ -21,18 +21,18 @@ const ConfirmButton = ({ children, onClick, ...props }: ConfirmButtonProps) => {
       </button>
 
       <Modal open={showModal} setOpen={setShowModal}>
-        <div className="overflow-hidden rounded">
-          <div className="p-6">
-            <h3 className="text-lg font-bold text-gray-700">Are you sure?</h3>
-            <p className="text-sm text-gray-500">
+        <div className='overflow-hidden rounded'>
+          <div className='p-6'>
+            <h3 className='text-lg font-bold text-gray-700'>Are you sure?</h3>
+            <p className='text-sm text-gray-500'>
               This action is irriversible, you're sure you want to do this?
             </p>
           </div>
-          <div className="px-6 py-4 space-x-6 bg-gray-50">
-            <button onClick={handleConfirm} className="btn-danger">
+          <div className='px-6 py-4 space-x-6 bg-gray-50'>
+            <button onClick={handleConfirm} className='btn-danger'>
               Delete
             </button>
-            <button onClick={() => setShowModal(false)} className="btn-text">
+            <button onClick={() => setShowModal(false)} className='btn-text'>
               Cancel
             </button>
           </div>
