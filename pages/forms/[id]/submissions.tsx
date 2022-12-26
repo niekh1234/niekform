0dc7c1113f4bd9715c5d0140f00bb5845bc8c2d9
Niek Hagen <niekhagen@hotmail.com>
2022-12-26 22:15:49 +0100

    (feat): empty fields state

--- pages/forms/[id]/submissions.tsx
+++ pages/forms/[id]/submissions.tsx
@@ -4,11 +4,13 @@ import { fetcher } from 'lib/client/api';
 import { Form } from 'lib/types';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
+import EmptyState from 'components/App/EmptyState';
+import Link from 'next/link';
 
 const FormSubmissions = () => {
   const router = useRouter();
   const { id } = router.query;
-  const { data, error: fetchError, isLoading, mutate } = useSWR('/api/admin/form/' + id, fetcher);
+  const { data, error: fetchError, isLoading } = useSWR('/api/admin/form/' + id, fetcher);
 
   if (isLoading) return <p>Loading...</p>;
 
@@ -17,14 +19,22 @@ const FormSubmissions = () => {
   if (fetchError || !form) return <p>Failed to load</p>;
 
   return (
-    <section className='max-w-5xl mx-auto'>
-      <h1 className='text-2xl font-bold'>{form.name}</h1>
+    <section className="max-w-5xl mx-auto">
+      <h1 className="text-2xl font-bold">{form.name}</h1>
 
       <FormTabs id={id as string}></FormTabs>
 
-      <div className='mt-12 overflow-hidden bg-white rounded-lg'>
-        <FormSubmissionsTable form={form}></FormSubmissionsTable>
-      </div>
+      {form.fields.length === 0 ? (
+        <EmptyState type="Field" className="mt-12">
+          <Link href={`/forms/${form.id}/fields`} className="btn-primary">
+            Create your first field
+          </Link>
+        </EmptyState>
+      ) : (
+        <div className="mt-12 overflow-hidden bg-white rounded-lg">
+          <FormSubmissionsTable form={form}></FormSubmissionsTable>
+        </div>
+      )}
     </section>
   );
 };
