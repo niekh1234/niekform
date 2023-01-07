8f3614a6e9a477c6a87bbc501d4342f3ee6e465f
Niek Hagen <niekhagen@hotmail.com>
2023-01-07 19:48:46 +0100

    (feat): submission search api

--- pages/api/admin/form/[id]/submissions.ts
+++ pages/api/admin/form/[id]/submissions.ts
@@ -3,6 +3,8 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import nextConnect from 'next-connect';
 import prisma from 'lib/prisma';
 import { ok, unauthorized } from 'lib/server/api';
+import { PrismaPromise } from '@prisma/client';
+import { Session, Submission } from 'lib/types';
 
 const DEFAULT_TAKE = 50;
 
@@ -14,43 +16,22 @@ export default nextConnect().get(async (req: NextApiRequest, res: NextApiRespons
   }
 
   const formId = req.query.id as string;
+  const searchValue = req.query.search as string;
 
-  const submissionsQuery = prisma.submission.findMany({
-    where: {
-      formId,
-      form: {
-        project: {
-          userId: session.id,
-        },
-      },
-    },
-    take: DEFAULT_TAKE,
-    orderBy: {
-      createdAt: 'desc',
-    },
-    skip: getSkip(req.query.page, DEFAULT_TAKE),
-  });
-
-  const totalSubmissionsQuery = prisma.submission.count({
-    where: {
-      formId,
-      form: {
-        project: {
-          userId: session.id,
-        },
-      },
-    },
-  });
+  const submissionsQuery = getQuery(formId, req.query.page, DEFAULT_TAKE, session, searchValue);
+  const totalSubmissionsQuery = getCountQuery(formId, session, searchValue);
 
   const [submissions, totalSubmissions] = await prisma.$transaction([
     submissionsQuery,
     totalSubmissionsQuery,
   ]);
 
+  const count = Number(totalSubmissions?.[0].count);
+
   const pagination = {
     page: req.query.page,
     take: DEFAULT_TAKE,
-    total: totalSubmissions,
+    total: count,
   };
 
   return ok(res, { submissions, pagination });
@@ -62,4 +43,42 @@ const getSkip = (page: any, take: number) => {
   }
 
   return (page - 1) * take;
+};
+
+const getQuery = (
+  formId: string,
+  page: any,
+  take: number,
+  session: Session,
+  searchQuery: string = ''
+): PrismaPromise<Submission[] | null> => {
+  // do a raw query on the jsonb column loosely matching the search query
+  return prisma.$queryRaw`
+      SELECT *
+      FROM "Submission"
+      WHERE "formId" = ${formId}
+      AND "formId" IN (SELECT "id" FROM "Form" WHERE "projectId" IN (SELECT "id" FROM "Project" WHERE "userId" = ${
+        session.id
+      }))
+      AND "data"::text ILIKE ${`%${searchQuery}%`}
+      ORDER BY "createdAt" DESC
+      LIMIT ${take}
+      OFFSET ${getSkip(page, take)}
+    `;
+};
+
+const getCountQuery = (
+  formId: string,
+  session: Session,
+  searchQuery: string = ''
+): PrismaPromise<any> => {
+  return prisma.$queryRaw`
+      SELECT COUNT(*)
+      FROM "Submission"
+      WHERE "formId" = ${formId}
+      AND "data"::text ILIKE ${`%${searchQuery}%`}
+      AND "formId" IN (SELECT "id" FROM "Form" WHERE "projectId" IN (SELECT "id" FROM "Project" WHERE "userId" = ${
+        session.id
+      }))
+    `;
 };
