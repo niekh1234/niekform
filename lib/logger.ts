64ecd1c3d3f4a9a3579bd9eed61eafd806b6acc6
Niek Hagen <niekhagen@hotmail.com>
2023-01-13 21:50:40 +0100

    (fix): do not require file logging

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,12 +1,18 @@
 import winston, { createLogger } from 'winston';
 
-export const logger = createLogger({
-  level: 'info',
-  format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
-  defaultMeta: { service: 'user-service' },
-  transports: [new winston.transports.File({ filename: 'debug.log', dirname: 'logs' })],
-});
+export const logger = process.env?.LOGGING_ENABLED
+  ? createLogger({
+      level: 'info',
+      format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+      defaultMeta: { service: 'user-service' },
+      transports: [new winston.transports.File({ filename: 'debug.log', dirname: 'logs' })],
+    })
+  : console;
 
-if (process.env.NODE_ENV !== 'production') {
+if (
+  process.env.NODE_ENV !== 'production' &&
+  process.env?.LOGGING_ENABLED &&
+  logger instanceof winston.Logger
+) {
   logger.add(new winston.transports.Console({ format: winston.format.simple() }));
 }
