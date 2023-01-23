6e0a05f42165acafa4fef073204e2494e9495281
Niek Hagen <niekhagen@hotmail.com>
2023-01-23 22:49:11 +0100

    (fix): type

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -9,7 +9,7 @@ export default NextAuth({
     EmailProvider({
       server: {
         host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
+        port: (process.env.EMAIL_SERVER_PORT as unknown as number) || 465,
         auth: {
           user: process.env.EMAIL_SERVER_USER,
           pass: process.env.EMAIL_SERVER_PASSWORD,
